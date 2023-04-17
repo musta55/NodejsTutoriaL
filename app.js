2c3343b1b5eda5c5b4ec73c087aedd3fb3579c82
musta55 <bsse1114@iit.du.ac.bd>
2023-04-17 16:21:23 +0600

    frontend with ejs

--- app.js
+++ app.js
@@ -1,29 +1,33 @@
-const express = require("express");
-// express app
+const express = require('express');
 
+// express app
 const app = express();
 
-// register view engine
-
-app.set("view engine", "ejs");
-
-// Listen for request
-
+// listen for requests
 app.listen(3001);
 
-app.get("/", (req, res) => {
-  res.render("index");
+// register view engine
+app.set('view engine', 'ejs');
+// app.set('views', 'myviews');
+
+app.get('/', (req, res) => {
+  const blogs = [
+    {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+  ];
+  res.render('index', { title: 'Home', blogs });
 });
 
-app.get("/about", (req, res) => {
-  res.render("about");
+app.get('/about', (req, res) => {
+  res.render('about', { title: 'About' });
 });
 
-app.get("/blogs/create", (req, res) => {
-  res.render("create");
+app.get('/blogs/create', (req, res) => {
+  res.render('create', { title: 'Create a new blog' });
 });
 
 // 404 page
-app.use((req, res) => { 
-  res.status(404).render("404");
+app.use((req, res) => {
+  res.status(404).render('404', { title: '404' });
 });
